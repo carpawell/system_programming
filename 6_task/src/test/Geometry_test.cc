6d0d8f307dcb7a783ad0cf47e3df2e0c1aa740d1
Pavel Karpiy <pasha.karpiy98@gmail.com>
2020-01-20 17:47:30 +0300

    feat(6_task): add sixth task

---
+++ 6_task/src/test/Geometry_test.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <myproject/Geometry.h>
+
+TEST(GEOMETRY, CIRCLE_TRAIT) {
+	float rad = 1;
+    auto c = Circle<float>(rad);
+
+    EXPECT_EQ(sizeof(rad), std::Trait<Circle<float>>::size(c));
+}
+
+TEST(GEOMETRY, RECTANGLE_TRAIT) {
+	double width = 1;
+	double height = 2;
+    auto r = Rectangle<double>(width, height);
+
+    EXPECT_EQ(sizeof(width) + sizeof(height), std::Trait<Rectangle<double>>::size(r));
+}
